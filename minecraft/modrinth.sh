6e998d77a7bec0cc9f75fcc197ee7fd21105aba6
Eleanor Young <developer@nausicaea.net>
2024-08-21 09:17:01 +0200

    Updated the skeleton a bit

--- minecraft/modrinth.sh
+++ minecraft/modrinth.sh
@@ -1,3 +1,35 @@
 #!/bin/ash
 
 set -xe
+
+download_datapack() {
+    printf 'unimplemented\n'
+    exit 1
+}
+
+download_mod() {
+    true
+}
+
+main() {
+    # Parse the subcommand
+    case $1 in
+        datapack) SUBCOMMAND="datapack"; break;;
+        mod) SUBCOMMAND="mod"; break;;
+        *) printf 'unimplemented\n'; exit 1;;
+    esac
+
+    # Ensure that the Modrinth API token is set
+    if [ "x$MODRINTH_PAT" = "x" ]; then
+        printf 'missing Modrinth personal authentication token (variable "MODRINTH_PAT")\n'
+        exit 1
+    fi
+
+    case $SUBCOMMAND in
+        datapack) download_datapack; break;;
+        mod) download_mod; break;;
+        *) exit 1;;
+    esac
+}
+
+main "$1"
