a7e768db9c3a7f4cd66cf5947edfae75289f5e0f
Eleanor Young <developer@nausicaea.net>
2024-09-16 21:58:57 +0200

    Changed the UID and GID of the minecraft user

--- minecraft/otel-mod/src/main/java/net/nausicaea/otel/Otel.java
+++ minecraft/otel-mod/src/main/java/net/nausicaea/otel/Otel.java
@@ -1,15 +1,23 @@
 package net.nausicaea.otel;
 
-import net.fabricmc.api.ModInitializer;
-
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.instrumentation.log4j.appender.v2_17.OpenTelemetryAppender;
+import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Otel implements ModInitializer {
+public class Otel implements PreLaunchEntrypoint {
     public static final Logger LOGGER = LoggerFactory.getLogger(Otel.class);
 
 	@Override
-	public void onInitialize() {
-		LOGGER.info("OpenTelemetry logging instrumentation loaded");
+	public void onPreLaunch() {
+		LOGGER.info("Obtain a reference to the global OpenTelemetry object (managed by the Java agent)");
+		OpenTelemetry otel = GlobalOpenTelemetry.get();
+
+		LOGGER.info("Install the OpenTelemetry appender in Log4j");
+		OpenTelemetryAppender.install(otel);
+
+		LOGGER.info("OpenTelemetry logging instrumentation is loaded");
 	}
 }
