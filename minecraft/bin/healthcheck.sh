0673f001c6f9c75d4b9a0d61b7c0d0f3874ce4bb
Eleanor Young <developer@nausicaea.net>
2025-08-14 20:07:39 +0200

    bugfix: fixed the healthcheck script

--- minecraft/bin/healthcheck.sh
+++ minecraft/bin/healthcheck.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
-MINECRAFT_SERVER_PORT=$(awk -F= '/server-port/ { gsub(/^[ \t]+|[ \t]+$/, "", $2); print $2 }' /etc/minecraft/server/server.properties)
+SERVER_ADDRESS='127.0.0.1'
+SERVER_PORT=$(awk -F= '/server-port/ { gsub(/^[ \t]+|[ \t]+$/, "", $2); print $2 }' /etc/minecraft/server/server.properties 2>/dev/null || printf '25565')
+PROTOCOL_VERSION='8106'
+SERVER_ADDRESS_LEN=$(printf '%s' "$SERVER_ADDRESS" | wc -c | awk '{ print $1 }')
+PACKET_LEN=$(printf '%02x' "$(( 1 + 2 + 1 + $SERVER_ADDRESS_LEN + 2 + 1 ))")
 
-PROTOCOL_VERSION=$(printf '8106' | xxd -r -p)
-SERVER_ADDRESS="127.0.0.1"
-SERVER_ADDRESS_LEN=$(printf '%02x' "$(printf '%s' "$SERVER_ADDRESS" | wc -c | awk '{ print $1 }')" | xxd -r -p)
-SERVER_PORT=$(printf '%02x' "$MINECRAFT_SERVER_PORT" | xxd -r -p)
-INTENT=$(printf '01' | xxd -r -p)
-TIMESTAMP=$(printf '%016x' "$(( $(date +%s) * 1000 ))" | xxd -r -p)
+HANDSHAKE_PACKET="${PACKET_LEN}00${PROTOCOL_VERSION}$(printf '%02x' "${SERVER_ADDRESS_LEN}")$(printf '127.0.0.1' | xxd -p)$(printf '%04x' "$SERVER_PORT")01"
+STATUS_PACKET="0100"
 
-HANDSHAKE_PACKET="\x10\x00$PROTOCOL_VERSION$SERVER_ADDRESS_LEN$SERVER_ADDRESS$SERVER_PORT$INTENT"
-STATUS_PACKET="\x01\x00"
-PING_PACKET="\x09\x01$TIMESTAMP"
+RESPONSE=$(printf '%s%s' "$HANDSHAKE_PACKET" "$STATUS_PACKET" | xxd -r -p | nc -w 2 -n "$SERVER_ADDRESS" "$SERVER_PORT" | tail -c +6)
+RESPONSE_LEN=$(printf '%s' $RESPONSE | wc -c | awk '{ print $1 }')
 
-SERVER_RESPONSE=$(printf '%s%s' "$HANDSHAKE_PACKET" "$STATUS_PACKET" | nc -w 2 -n 127.0.0.1 "$MINECRAFT_SERVER_PORT")
-
-echo "Response: $SERVER_RESPONSE"
-exit 1
+if [ "$RESPONSE_LEN" -gt 0 ]; then
+    printf '%s\n' "$RESPONSE"
+    exit 0
+else
+    printf 'empty response from server\n'
+    exit 1
+fi
